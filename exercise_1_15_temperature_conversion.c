75e9283a176bddd11872c6a9e7da98ae6222285b
pezy <urbancpz@gmail.com>
2017-11-01 23:24:09 +0800

    fixed 1_15

--- exercise_1_15_temperature_conversion.c
+++ exercise_1_15_temperature_conversion.c
@@ -5,12 +5,12 @@ float Fahr2Celsius(float fahr);
 main()
 {
 	int i;
-	
+
 	for (i = 0; i <= 300; i += 20)
-		printf("%3.0f %6.1f\n", i, Fahr2Celsius(i));
+		printf("%3d %6.1f\n", i, Fahr2Celsius(i));
 }
 
-float Fahr2Celsius(float fahr) 
+float Fahr2Celsius(float fahr)
 {
 	return (5.0f / 9.0f)*(fahr - 32.0f);
 }
